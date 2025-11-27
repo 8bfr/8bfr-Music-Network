3e77648203ae406bd3ccc326193d241171d517b8
8bfr <8bfr.music@gmail.com>
2025-11-27 14:23:10 -0500

    Update carrie-closet-data.js

--- docs/carrie-closet-data.js
+++ docs/carrie-closet-data.js
@@ -43,7 +43,7 @@
       img: base + "/hair/straight/female_straight_blonde.png",
       scale: 0.95,
       offsetX: 0,
-      offsetY: -55
+      offsetY: -40
     }),
     item({
       id: "f_hair_straight_brown",
