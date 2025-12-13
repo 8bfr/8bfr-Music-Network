3bb169e9eb40c1c3ed7d58cb960b92b954570cef
8bfr <8bfr.music@gmail.com>
2025-12-13 13:50:11 -0500

    Update carrie-closet-data.js

--- docs/carrie-closet-data.js
+++ docs/carrie-closet-data.js
@@ -303,7 +303,7 @@
       name: "8BFR Tank Top",
       label: "unisex tank",
       coins: 15,
-      img: base + "/unisex/cloths/unisex_tank-top_v4.png",
+      img: base + "/unisex/cloths/unisex_tank-top_v5.png",
       scale: 0.92,
       offsetX: 0,
       offsetY: 6
