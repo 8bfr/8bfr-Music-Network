9e533e4083c44e8ec733944771ad1e1f41a165df
8bfr <8bfr.music@gmail.com>
2026-01-18 22:53:23 -0500

    Update scripts.js

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,4 +1,4 @@
-javascript
+```javascript
 // ===== SUPABASE CONFIGURATION =====
 const SUPABASE_URL = 'https://novbuvwpjnxwwvdekjhr.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5vdmJ1dndwam54d3d2ZGVramhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjExODkxODUsImV4cCI6MjA3Njc2NTE4NX0.1UUkdGafh6ZplAX8hi7Bvj94D2gvFQZUl0an1RvcSA0';
@@ -171,4 +171,5 @@ supabase.auth.onAuthStateChange((event, session) => {
         }
     }
 });
+```
 
